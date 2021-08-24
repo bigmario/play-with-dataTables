40eb0b48d8a119ee156149b8cd7fd408336e3375
Mario Castro <mariocastro.pva@gmail.com>
2021-08-23 21:41:10 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* $(document).ready(function() {
+    var table = $('#example').DataTable({
+        searchBuilder: true
+    });
+    table.searchBuilder.container().prependTo(table.table().container());
+} ); */
+
+/* $(document).ready(function() {
+    $('#example').DataTable();
+} ); */
+
+$(document).ready(function() {
+    $('#example').DataTable({
+        dom: 'Qfrtip'
+    });
+} );
